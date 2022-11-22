4d258d1449c875dd820905ac52f34db6e4c4b559
MarMariam <m.arakelyan@omedia.ge>
2022-11-22 20:13:31 +0100

    Error message in gallery page

--- src/app/photo-gallery/component/photo-gallery/photo-gallery.component.ts
+++ src/app/photo-gallery/component/photo-gallery/photo-gallery.component.ts
@@ -4,10 +4,10 @@ import { PhotoDetail } from '../../model/photo-detail.model';
 import { PhotoGalleryService } from '../../service/photo-gallery.service';
 
 import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
-
-// RxJS
 import { of } from 'rxjs';
 import { catchError, filter, finalize, map, pairwise, throttleTime } from 'rxjs/operators';
+
+import { PhotoInfo } from 'src/app/helper/enum/photoInfo';
 
 @Component({
   selector: 'app-photo-gallery',
@@ -20,41 +20,29 @@ import { catchError, filter, finalize, map, pairwise, throttleTime } from 'rxjs/
 export class PhotoGalleryComponent implements OnInit, AfterViewInit, OnDestroy {
 
   private subscription: any;
-  error = false;
-
-  currentPage = 1;
   images = new Array<PhotoDetail>();
-  loading = true;
   isMobile = true;
-
-    errorMsg = '';
-    isLoading = false;
+  errorMsg = '';
+  isLoading = false;
 
 
   @ViewChild('scroller') scroller!: CdkVirtualScrollViewport;
-  public pageNumber1: number = 1;
+  public pageNumber: number = PhotoInfo.LOADINGPAGENUMBER;
 
   constructor(private photoService: PhotoGalleryService, private router: Router, private ngZone: NgZone,) {}
 
   ngOnInit(): void {
-//       this.images = [];
-this.isMobile = window.innerWidth <= 576;
-this.loadMore();
+    this.isMobile = window.innerWidth <= 576;
+    this.loadMore();
   }
 
     ngAfterViewInit() {
-      // Whenever the virtual scroller scrolls
       this.scroller.elementScrolled().pipe(
-        // Get the scroll offset from the bottom
         map(() => this.scroller.measureScrollOffset('bottom')),
-        // Get emitted values in pairs: [previousValue, currentValue]
         pairwise(),
-        // Continue emission if movement is a downward scroll and list is near the bottom
         filter(([y1, y2]) => (!this.isLoading && y2 < y1 && (this.isMobile ? y2 < 272 : y2 < 368))),
-        // Lets a value pass, but ignore next values for the next 200ms
         throttleTime(200)
       ).subscribe(() => {
-        // Virtual scroller runs outside Angular zone, let API call reenter Angular zone
         this.ngZone.run(() => {
           this.loadMore();
         });
@@ -72,18 +60,25 @@ this.loadMore();
     }
 
     private loadMore() {
-//       this.isLoading = true;
-      this.photoService.getPhotos(this.pageNumber1, 10).subscribe((photos1: any) => {
-          if (photos1.length > 0) {
-            this.images = this.images.concat(photos1);
-            this.pageNumber1++;
-//             this.errorMsg = '';
+      this.isLoading = true;
+      this.photoService.getPhotos(this.pageNumber, PhotoInfo.LOADINGLIMITNUMBER)
+      .pipe(
+            catchError(errorMsg => {
+              this.errorMsg = errorMsg;
+              return of([]);
+            }),
+            finalize(() => this.isLoading = false)
+          )
+      .subscribe((photoItem: any) => {
+          if (photoItem.length > 0) {
+            this.images = this.images.concat(photoItem);
+            this.pageNumber++;
+            this.errorMsg = '';
           }
         });
     }
 
   imgDetails(image: any) {
-  console.log(typeof image, 'image type')
     sessionStorage.setItem('id', image.id);
     this.router.navigate(['/detail/' + image.id]);
   }
